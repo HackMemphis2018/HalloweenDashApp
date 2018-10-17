b04dd179badd182cb6831d374ca8de13bc96aa38
Arielle Morris <ariellemorris@arielles-mbp>
2018-10-16 20:10:56 -0500

    Initial Commit

---
+++ DashProto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DashProto
+//
+//  Created by Arielle Morris on 10/16/18.
+//  Copyright © 2018 Arielle Morris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
